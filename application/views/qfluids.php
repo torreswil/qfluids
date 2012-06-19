8b1f6c1deb0515f3d887b06d269753a0cfdd0c73
José Daniel Paternina <josedaniel.paterninasoto@gmail.com>
2012-06-19 13:21:08 -0500

    FINALIZADO FORMULAIO DE ESTADO MECANICO

--- application/views/qfluids.php
+++ application/views/qfluids.php
@@ -305,6 +305,55 @@
 					</tr>
 				</table>
 			</fieldset>
+
+			<!-- MUD PUMP -->
+			<fieldset>
+				<legend>Mud pump</legend>
+				<table>
+					<tr>
+						<td></td>
+						<td class="label_m"><label>PUMP 1</label></td>
+						<td class="label_m"><label>PUMP 2</label></td>
+						<td class="label_m"><label>PUMP 3</label></td>
+						<td class="label_m"><label>PUMP 4</label></td>
+					</tr>
+					<tr>
+						<td class="label_m"><label>LINER/STK</label></td>
+						<td><input type="text" /></td>
+						<td><input type="text" /></td>
+						<td><input type="text" /></td>
+						<td><input type="text" /></td>
+					</tr>
+					<tr>
+						<td class="label_m"><label>GAL/STK</label></td>
+						<td><input type="text" /></td>
+						<td><input type="text" /></td>
+						<td><input type="text" /></td>
+						<td><input type="text" /></td>
+					</tr>
+					<tr>
+						<td class="label_m"><label>SPM/STK</label></td>
+						<td><input type="text" /></td>
+						<td><input type="text" /></td>
+						<td><input type="text" /></td>
+						<td><input type="text" /></td>
+					</tr>
+					<tr>
+						<td class="label_m"><label>GAL</label></td>
+						<td><input type="text" /></td>
+						<td><input type="text" /></td>
+						<td><input type="text" /></td>
+						<td><input type="text" /></td>
+					</tr>
+					<tr>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td class="label_m"><label>TOTAL GAL:</label></td>
+						<td><input type="text" /></td>
+					</tr>
+				</table>
+			</fieldset>
 		</div>
 
 	</div>
