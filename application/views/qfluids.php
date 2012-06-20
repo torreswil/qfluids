3f7013a25245ca671144891cf0504f7a0d5285ab
José Daniel Paternina <josedaniel.paterninasoto@gmail.com>
2012-06-20 12:16:43 -0500

    modificaciones de estilo profundas

--- application/views/qfluids.php
+++ application/views/qfluids.php
@@ -13,22 +13,24 @@
 	</div>
 	
 	<div class="panels">
-		<div id="project_info">
-			<table>
-				<tr>
-					<td><strong>SPUD DAY:</strong><br />2012-06-19</td>
-					<td width="25"></td>
-					<td><strong>DATE:</strong><br />2012-06-19</td>
-					<td width="25"></td>
-					<td><strong>Operadora:</strong><br />QMAX.DEV</td>
-					<td width="25"></td>
-					<td><strong>Report #:</strong><br />0001</td>
-					<td width="25"></td>
-					<td><strong>Well:</strong><br /><input type="text" placeholder="Nombre del Pozo" /></td>
-					<td width="25"></td>
-					<td><strong>Rig:</strong><br /><input type="text" placeholder="Nombre del Taladro" /></td>
-				</tr>
-			</table>	
+		<div class="project_info_wrapper">
+			<div id="project_info">
+				<table>
+					<tr>
+						<td><strong>SPUD DAY:</strong><br /><span style="text-transform:uppercase;color:#008040;font-weight:bold;">2012-06-19</span></td>
+						<td width="25"></td>
+						<td><strong>DATE:</strong><br /><span style="text-transform:uppercase;color:#008040;font-weight:bold;">2012-06-20</span></td>
+						<td width="25"></td>
+						<td><strong>Report#:</strong><br /><span style="text-transform:uppercase;color:#008040;font-weight:bold;">0001</span></td>
+						<td width="150"></td>
+						<td><strong>Operadora:</strong><br /><span style="text-transform:uppercase;color:#008040;font-weight:bold;">ECOPETROL</span></td>
+						<td width="25"></td>
+						<td><strong>Well:</strong><br /><input type="text" placeholder="Nombre del Pozo" /></td>
+						<td width="25"></td>
+						<td><strong>Rig:</strong><br /><input type="text" placeholder="Nombre del Taladro" /></td>
+					</tr>
+				</table>	
+			</div>
 		</div>
 		
 		<!-- PANELES DE DATOS DE ENTRADA-->
