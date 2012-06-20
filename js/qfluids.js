7ae84f52ca49be17a73b64e62148c0734a0ffeda
José Daniel Paternina <josedaniel.paterninasoto@gmail.com>
2012-06-20 10:49:08 -0500

    sidebar posicionada dinamicamete

--- js/qfluids.js
+++ js/qfluids.js
@@ -1,4 +1,21 @@
+function sidebar_position(){
+	var win_width = $(window).width();
+	var x_pos = (win_width - 1200)/2;
+	if(win_width >= 1200){
+		$('.sidebar').css('left',x_pos).show();
+	}else{
+		$('.sidebar').css('left',0).show();
+	}
+}
+
+$(window).resize(function(){
+	console.log($('.qfluids_wrapper').position().left);
+	sidebar_position();	
+});
+
 $(document).ready(function(){
+	sidebar_position();
+
 	//CLICK EN UN ENLACE DE LA BARRA LATERAL
 	$('.nav_links a').click(function(e){
 		e.preventDefault();
