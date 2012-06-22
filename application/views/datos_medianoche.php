79e01aad192557661e0b4cb441ff1cadf1361d51
Freddy Rojas <freddygonzalorojas@hotmail.com>
2012-06-22 15:20:07 -0500

    datos de media noche grilla

--- application/views/datos_medianoche.php
+++ application/views/datos_medianoche.php
@@ -1,197 +1,216 @@
 <div class="this_panel" id="datos_medianoche">
 	<h2>Datos de Media Noche</h2>
 	
-	<fieldset>
-		<table>
-			<tr>
-				<td class="label_m"><label>Activity:</label></td><td><input type="text" /></td>
-				<td class="label_m"><label>Formation</label></td><td><input type="text" /></td>
-			</tr>
-		</table>
-		<fieldset>
-			<legend>Parámetros de Perforación</legend>
-			<table>
-				<tr>
-					<td class="label_m"><label>RPM:</label></td><td><input type="text" /></td>
-					<td class="label_m"><label>WOB:</label></td><td><input type="text" /></td>
-				</tr>
-				<tr>
-					<td class="label_m"><label>Circ. Pres:</label></td><td><input type="text" /></td>
-					<td class="label_m"><label>GPM:</label></td><td><input type="text" /></td>
-				</tr>
-			</table>
-			<table>
-				<tr>
-					<td class="label_m"><label>Cir. time on Bottom-h:</label></td><td><input type="text" /></td>
-				</tr>
-				<tr>	
-					<td class="label_m"><label>Dayly avg ROP, ft/h:</label></td><td><input type="text" /></td>
-				</tr>
-				<tr>	
-					<td class="label_m"><label>Average cavings while, bbl/h:</label></td><td><input type="text" /></td>
-				</tr>
-				<tr>	
-					<td class="label_m"><label>Average cutting while bbl/h:</label></td><td><input type="text" /></td>
-				</tr>
-			</table>
-		</fieldset>
-		<fieldset>
-			<legend>Survey</legend>
-			<table>
-				<tr>
-					<td class="label_m"><label>MD:</label></td><td><input type="text" /></td>
-				</tr>
-				<tr>	
-					<td class="label_m"><label>TVD:</label></label></td><td><input type="text" /></td>
-				</tr>
-				<tr>	
-					<td class="label_m"><label>INCL:</label></td><td><input type="text" /></td>
-				</tr>
-				<tr>	
-					<td class="label_m"><label>AZIMUT:</label></td><td><input type="text" /></td>
-				</tr>
-				<tr>	
-					<td class="label_m"><label>DOG LEG:</label></td><td><input type="text" /></td>
-				</tr>
-			</table>
-		</fieldset>
-	</fieldset>
 
-	<fieldset>
-		<legend>Comentarios</legend>
-		<table>
-			<tr>
-				<td>
-					<textarea></textarea>
-				</td>
-			</tr>
-		</table>
-	</fieldset>
 
-	<fieldset>
-		<legend>Personal y Charla Diaria</legend>
-		<table>
-			<tr>
-				<td class="label_m"><label>Costo Ingeniería [hoy]</label></td><td><input type="text" /></td>
-			</tr>
-			<tr>
-				<td class="label_m"><label># Ingenieros a reportar</label></td><td><input type="text" /></td>
-			</tr>
-			<tr>	
-				<td class="label_m"><label>Costo ingeniería acumulado</label></td><td><input type="text" disabled="disabled" /></td>
-			</tr>
-		</table>
-		<fieldset>
-			<legend>Personal</legend>
-			<table>
-				<tr>
-					<td class="label_m"><label>Ing. Fluidos 1:</label></td><td><input type="text" /></td>
-				</tr>
-				<tr>
-					<td class="label_m"><label>Ing. Fluidos 2:</label></td><td><input type="text" /></td>
-				</tr>
-				<tr>
-					<td class="label_m"><label>Ing. CO-MAN:</label></td><td><input type="text" /></td>
-				</tr>
-				<tr>
-					<td class="label_m"><label>Ing. CO-MAN:</label></td><td><input type="text" /></td>
-				</tr>
-				<tr>
-					<td class="label_m"><label>Pusher:</label></td><td><input type="text" /></td>
-				</tr>
-				<tr>
-					<td class="label_m"><label>Charla Diaria:</label></td><td><input type="text" /></td>
-				</tr>
-			</table>
-		</fieldset>
-	</fieldset>
 
-	<fieldset>
-		<legend>Equipos CTS</legend>
-		<fieldset>
-			<legend>Shakers</legend>
-			<table>
-				<tr>
-					<td></td>
-					<td class="label_m"><label>1</label></td>
-					<td class="label_m"><label>2</label></td>
-					<td class="label_m"><label>3</label></td>
-					<td class="label_m"><label>4</label></td>
-				</tr>
-				<tr>
-					<td class="label_m"><label>Screens:</label></td>
-					<td><input type="text" /></td>
-					<td><input type="text" /></td>
-					<td><input type="text" /></td>
-					<td><input type="text" /></td>
-				</tr>
-				<tr>
-					<td class="label_m"><label>Hours</label></td>
-					<td><input type="text" /></td>
-					<td><input type="text" /></td>
-					<td><input type="text" /></td>
-					<td><input type="text" /></td>
-				</tr>
-			</table>
-		</fieldset>
+<fieldset>
+	<table style="float:right;" >
+		<tr>
+			<td class="label_m"><label>Activity:</label></td><td><input type="text" /></td>
+			<td class="label_m"><label>Formation</label></td><td><input type="text" /></td>
+		</tr>
+	</table>
+</fieldset>
 		
-		<fieldset>
-			<legend>Skalpers</legend>
-			<table>
-				<tr>
-					<td class="label_m"><label>Screens:</label></td>
-					<td><input type="text" /></td>
-				</tr>
-				<tr>
-					<td class="label_m"><label>Hours</label></td>
-					<td><input type="text" /></td>
-				</tr>
-			</table>
-		</fieldset>
+<table class="super_grid" style="width:930px;">
+	<tr>
+		<td>
+			<fieldset>
+				<legend>Parámetros de Perforación</legend>
+				<table style="float:left;">
+					<tr>
+						<td class="label_m"><label>RPM:</label></td><td><input type="text" /></td>
+						<td class="label_m"><label>WOB:</label></td><td><input type="text" /></td>
+					</tr>
+					<tr>
+						<td class="label_m"><label>Circ. Pres:</label></td><td><input type="text" /></td>
+						<td class="label_m"><label>GPM:</label></td><td><input type="text" /></td>
+					</tr>
+				</table>
+				<table style="float:left;">
+					<tr>
+						<td class="label_m"><label>Cir. time on Bottom-h:</label></td><td><input type="text" /></td>
+					</tr>
+					<tr>	
+						<td class="label_m"><label>Dayly avg ROP, ft/h:</label></td><td><input type="text" /></td>
+					</tr>
+					<tr>	
+						<td class="label_m"><label>Average cavings while, bbl/h:</label></td><td><input type="text" /></td>
+					</tr>
+					<tr>	
+						<td class="label_m"><label>Average cutting while bbl/h:</label></td><td><input type="text" /></td>
+					</tr>
+				</table>
+			</fieldset>
+		<td>	
+			<fieldset>
+				<legend>Survey</legend>
+				<table style="float:left;">
+					<tr>
+						<td class="label_m"><label>MD:</label></td><td><input type="text" /></td>
+					</tr>
+					<tr>	
+						<td class="label_m"><label>TVD:</label></label></td><td><input type="text" /></td>
+					</tr>
+					<tr>	
+						<td class="label_m"><label>INCL:</label></td><td><input type="text" /></td>
+					</tr>
+					<tr>	
+						<td class="label_m"><label>AZIMUT:</label></td><td><input type="text" /></td>
+					</tr>
+					<tr>	
+						<td class="label_m"><label>DOG LEG:</label></td><td><input type="text" /></td>
+					</tr>
+				</table>
+			</fieldset>								
+		</td>
+	</tr>
+	<tr>	
+		<td>
+			<fieldset>
+				<legend>Comentarios</legend>
+				<table style="float:left;">
+					<tr>
+						<td>
+							<textarea></textarea>
+						</td>
+					</tr>
+				</table>
+			</fieldset>
+		</td>
+	</tr>
+	<tr>
+		<td>
+			<fieldset>
+				<legend>Personal y Charla Diaria</legend>
+				<table style="float:left;">
+					<tr>
+						<td class="label_m"><label>Costo Ingeniería [hoy]</label></td><td><input type="text" /></td>
+					</tr>
+					<tr>
+						<td class="label_m"><label># Ingenieros a reportar</label></td><td><input type="text" /></td>
+					</tr>
+					<tr>	
+						<td class="label_m"><label>Costo ingeniería acumulado</label></td><td><input type="text" disabled="disabled" /></td>
+					</tr>
+				</table>
+				<fieldset>
+					<legend>Personal</legend>
+					<table style="float:left;">
+						<tr>
+							<td class="label_m"><label>Ing. Fluidos 1:</label></td><td><input type="text" /></td>
+						</tr>
+						<tr>
+							<td class="label_m"><label>Ing. Fluidos 2:</label></td><td><input type="text" /></td>
+						</tr>
+						<tr>
+							<td class="label_m"><label>Ing. CO-MAN:</label></td><td><input type="text" /></td>
+						</tr>
+						<tr>
+							<td class="label_m"><label>Ing. CO-MAN:</label></td><td><input type="text" /></td>
+						</tr>
+						<tr>
+							<td class="label_m"><label>Pusher:</label></td><td><input type="text" /></td>
+						</tr>
+						<tr>
+							<td class="label_m"><label>Charla Diaria:</label></td><td><input type="text" /></td>
+						</tr>
+					</table>
+				</fieldset>
+			</fieldset>
+		</td>
+		<td>
+			<fieldset>
+				<legend>Equipos CTS</legend>
+				<fieldset>
+					<legend>Shakers</legend>
+					<table style="float:left;">
+						<tr>
+							<td></td>
+							<td class="label_m"><label>1</label></td>
+							<td class="label_m"><label>2</label></td>
+							<td class="label_m"><label>3</label></td>
+							<td class="label_m"><label>4</label></td>
+						</tr>
+						<tr>
+							<td class="label_m"><label>Screens:</label></td>
+							<td><input type="text" /></td>
+							<td><input type="text" /></td>
+							<td><input type="text" /></td>
+							<td><input type="text" /></td>
+						</tr>
+						<tr>
+							<td class="label_m"><label>Hours</label></td>
+							<td><input type="text" /></td>
+							<td><input type="text" /></td>
+							<td><input type="text" /></td>
+							<td><input type="text" /></td>
+						</tr>
+					</table>
+				</fieldset>
+				
+				<fieldset>
+					<legend>Skalpers</legend>
+					<table style="float:left;">
+						<tr>
+							<td class="label_m"><label>Screens:</label></td>
+							<td><input type="text" /></td>
+						</tr>
+						<tr>
+							<td class="label_m"><label>Hours</label></td>
+							<td><input type="text" /></td>
+						</tr>
+					</table>
+				</fieldset>
 
-		<fieldset>
-			<legend>Mud Cleaner</legend>
-			<table>
-				<tr>
-					<td class="label_m"><label>Screens:</label></td>
-					<td><input type="text" /></td>
-					<td class="label_m"><label>Desilter:</label></td>
-					<td><input type="text" /></td>
-				</tr>
-				<tr>
-					<td class="label_m"><label>Desander:</label></td>
-					<td><input type="text" /></td>
-					<td class="label_m"><label>Hours:</label></td>
-					<td><input type="text" /></td>
-				</tr>
-			</table>
-		</fieldset>
-		<fieldset>
-			<legend>Centrifugues</legend>
-			<table>
-				<tr>
-					<td class="label_m"><label>Equipment</label></td>
-					<td class="label_m"><label>Underflow</label></td>
-					<td class="label_m"><label>Overflow</label></td>
-					<td class="label_m"><label>GPM</label></td>
-					<td class="label_m"><label>Hours</label></td>
-				</tr>
-				<tr>
-					<td><input type="text" /></td>
-					<td><input type="text" /></td>
-					<td><input type="text" /></td>
-					<td><input type="text" /></td>
-					<td><input type="text" /></td>
-				</tr>
-				<tr>
-					<td><input type="text" /></td>
-					<td><input type="text" /></td>
-					<td><input type="text" /></td>
-					<td><input type="text" /></td>
-					<td><input type="text" /></td>
-				</tr>
-			</table>
-		</fieldset>
-	</fieldset>
-	
+				<fieldset>
+					<legend>Mud Cleaner</legend>
+					<table style="float:left;">
+						<tr>
+							<td class="label_m"><label>Screens:</label></td>
+							<td><input type="text" /></td>
+							<td class="label_m"><label>Desilter:</label></td>
+							<td><input type="text" /></td>
+						</tr>
+						<tr>
+							<td class="label_m"><label>Desander:</label></td>
+							<td><input type="text" /></td>
+							<td class="label_m"><label>Hours:</label></td>
+							<td><input type="text" /></td>
+						</tr>
+					</table>
+				</fieldset>
+				<fieldset>
+					<legend>Centrifugues</legend>
+					<table style="float:left;">
+						<tr>
+							<td class="label_m"><label>Equipment</label></td>
+							<td class="label_m"><label>Underflow</label></td>
+							<td class="label_m"><label>Overflow</label></td>
+							<td class="label_m"><label>GPM</label></td>
+							<td class="label_m"><label>Hours</label></td>
+						</tr>
+						<tr>
+							<td><input type="text" /></td>
+							<td><input type="text" /></td>
+							<td><input type="text" /></td>
+							<td><input type="text" /></td>
+							<td><input type="text" /></td>
+						</tr>
+						<tr>
+							<td><input type="text" /></td>
+							<td><input type="text" /></td>
+							<td><input type="text" /></td>
+							<td><input type="text" /></td>
+							<td><input type="text" /></td>
+						</tr>
+					</table>
+				</fieldset>
+			</fieldset>
+		</td>
+	</tr>	
+</table>	
+
+		
 </div>
