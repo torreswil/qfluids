76736715c6a61a528ecae97d82844631a7f1bdfb
Freddy Rojas <freddygonzalorojas@hotmail.com>
2012-06-22 22:56:34 -0500

    columnas proiedades de lodo

--- application/views/propiedades_lodo.php
+++ application/views/propiedades_lodo.php
@@ -12,259 +12,276 @@
 			</tr>
 		</table>
 	</fieldset>
+<table style="width:930px;">
+	<tr>
+		<td>
+			<fieldset>
+				<legend>Fisicas</legend>
+				<table>
+					<tr>
+						<td></td>
+						<td class="label_m"><label>Program</label></td>
+						<td class="label_m"><label>6:00</label></td>
+						<td class="label_m"><label>14:00</label></td>
+						<td class="label_m"><label>23:00</label></td>
+					</tr>
+					<tr>
+						<td class="label_m"><label>depth</label></td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+					</tr>
+					<tr>
+						<td class="label_m"><label>pit flow line</label></td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+					</tr>
+					<tr>
+						<td class="label_m"><label>flowline temp ºF</label></td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+					</tr>
+					<tr>
+						<td class="label_m"><label>mud weight ppg</label></td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+					</tr>
+					<tr>
+						<td class="label_m"><label>Funnel viscocity</label></td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+					</tr>
+					<tr>
+						<td class="label_m"><label>API filter cake</label></td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+					</tr>
+					<tr>
+						<td class="label_m"><label>Sand</label></td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+					</tr>
+					<tr>
+						<td class="label_m"><label>Lubricant</label></td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+					</tr>
+					<tr>
+						<td class="label_m"><label>Inhibidrol</label></td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+					</tr>
+				</table>
+			</fieldset>
+		</td>
+		<td>
+			<fieldset style="height:266px;">
+				<legend>Reologica</legend>
+				<table>
+					<tr>
+						<td></td>
+						<td class="label_m"><label>Program</label></td>
+						<td class="label_m"><label>6:00</label></td>
+						<td class="label_m"><label>14:00</label></td>
+						<td class="label_m"><label>23:00</label></td>
+					</tr>
+					<tr>
+						<td class="label_m"><label>600/300</label></td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+					</tr>
+					<tr>
+						<td class="label_m"><label>20/100</label></td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+					</tr>
+					<tr>
+						<td class="label_m"><label>6/3</label></td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+					</tr>
+					<tr>
+						<td class="label_m"><label>pv</label></td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+					</tr>
+					<tr>
+						<td class="label_m"><label>yp</label></td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+					</tr>
+					<tr>
+						<td class="label_m"><label>YS</label></td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+					</tr>
+					<tr>
+						<td class="label_m"><label>GEL 10", 10', 30'</label></td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+					</tr>	
+				</table>
+			</fieldset>
+		</td>
+	</tr>
+	<tr>
+		<td>
+			<fieldset>
+				<legend>Químicas</legend>
+				<table>
+					<tr>
+						<td></td>
+						<td class="label_m"><label>Program</label></td>
+						<td class="label_m"><label>6:00</label></td>
+						<td class="label_m"><label>14:00</label></td>
+						<td class="label_m"><label>23:00</label></td>
+					</tr>
+					<tr>
+						<td>
+							<td class="label_m"><label>pH METER</label></td>	
+						</td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+					</tr>
+					<tr>
+						<td>
+							<td class="label_m"><label>PM</label></td>	
+						</td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+					</tr>
+					<tr>
+						<td>
+							<td class="label_m"><label>PF/MF</label></td>	
+						</td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+					</tr>
+					<tr>
+						<td>
+							<td class="label_m"><label>MBT</label></td>	
+						</td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+					</tr>
+					<tr>
+						<td>
+							<td class="label_m"><label>CHLORIDES</label></td>	
+						</td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+					</tr>
+					<tr>
+						<td>
+							<td class="label_m"><label>Ca++</label></td>	
+						</td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+					</tr>
+					<tr>
+						<td>
+							<td class="label_m"><label>% water</label></td>	
+						</td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+					</tr>
+					<tr>
+						<td>
+							<td class="label_m"><label>% oil</label></td>	
+						</td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+						<td><input type="text"></td>
+					</tr>
+				</table>
+			</fieldset>
+		</td>
+		<td>
+			<fieldset style="height:241px;">
+				<legend>Others</legend>
+				<table>
+					<tr>
+						<td class="label_m"><label>MUD PROPERTIES<label></td>
+						<td class="label_m"><label>PROGRAM</label></td>
+						<td class="label_m"><label>6:00</label></td>
+						<td class="label_m"><label>14:00</label></td>
+						<td class="label_m"><label>23:00</label></td>
+					</tr>
+					<tr>
+						<td><input type="text" /></td>
+						<td><input type="text" /></td>
+						<td><input type="text" /></td>
+						<td><input type="text" /></td>
+						<td><input type="text" /></td>
+					</tr>
+					<tr>
+						<td><input type="text" /></td>
+						<td><input type="text" /></td>
+						<td><input type="text" /></td>
+						<td><input type="text" /></td>
+						<td><input type="text" /></td>
+					</tr>
+					<tr>
+						<td><input type="text" /></td>
+						<td><input type="text" /></td>
+						<td><input type="text" /></td>
+						<td><input type="text" /></td>
+						<td><input type="text" /></td>
+					</tr>
+				</table>
+			</fieldset>
+		</td>
+	</tr>
+</table>	
 	
-	<fieldset>
-		<legend>Fisicas</legend>
-		<table>
-			<tr>
-				<td></td>
-				<td class="label_m"><label>Program</label></td>
-				<td class="label_m"><label>6:00</label></td>
-				<td class="label_m"><label>14:00</label></td>
-				<td class="label_m"><label>23:00</label></td>
-			</tr>
-			<tr>
-				<td class="label_m"><label>depth</label></td>
-				<td><input type="text"></td>
-				<td><input type="text"></td>
-				<td><input type="text"></td>
-				<td><input type="text"></td>
-			</tr>
-			<tr>
-				<td class="label_m"><label>pit flow line</label></td>
-				<td><input type="text"></td>
-				<td><input type="text"></td>
-				<td><input type="text"></td>
-				<td><input type="text"></td>
-			</tr>
-			<tr>
-				<td class="label_m"><label>flowline temp ºF</label></td>
-				<td><input type="text"></td>
-				<td><input type="text"></td>
-				<td><input type="text"></td>
-				<td><input type="text"></td>
-			</tr>
-			<tr>
-				<td class="label_m"><label>mud weight ppg</label></td>
-				<td><input type="text"></td>
-				<td><input type="text"></td>
-				<td><input type="text"></td>
-				<td><input type="text"></td>
-			</tr>
-			<tr>
-				<td class="label_m"><label>Funnel viscocity</label></td>
-				<td><input type="text"></td>
-				<td><input type="text"></td>
-				<td><input type="text"></td>
-				<td><input type="text"></td>
-			</tr>
-			<tr>
-				<td class="label_m"><label>API filter cake</label></td>
-				<td><input type="text"></td>
-				<td><input type="text"></td>
-				<td><input type="text"></td>
-				<td><input type="text"></td>
-			</tr>
-			<tr>
-				<td class="label_m"><label>Sand</label></td>
-				<td><input type="text"></td>
-				<td><input type="text"></td>
-				<td><input type="text"></td>
-				<td><input type="text"></td>
-			</tr>
-			<tr>
-				<td class="label_m"><label>Lubricant</label></td>
-				<td><input type="text"></td>
-				<td><input type="text"></td>
-				<td><input type="text"></td>
-				<td><input type="text"></td>
-			</tr>
-			<tr>
-				<td class="label_m"><label>Inhibidrol</label></td>
-				<td><input type="text"></td>
-				<td><input type="text"></td>
-				<td><input type="text"></td>
-				<td><input type="text"></td>
-			</tr>
-		</table>
-	</fieldset>
-	<fieldset>
-		<legend>Reologica</legend>
-		<table>
-			<tr>
-				<td></td>
-				<td class="label_m"><label>Program</label></td>
-				<td class="label_m"><label>6:00</label></td>
-				<td class="label_m"><label>14:00</label></td>
-				<td class="label_m"><label>23:00</label></td>
-			</tr>
-			<tr>
-				<td class="label_m"><label>600/300</label></td>
-				<td><input type="text"></td>
-				<td><input type="text">/<input type="text"></td>
-				<td><input type="text">/<input type="text"></td>
-				<td><input type="text">/<input type="text"></td>
-			</tr>
-			<tr>
-				<td class="label_m"><label>20/100</label></td>
-				<td><input type="text"></td>
-				<td><input type="text">/<input type="text"></td>
-				<td><input type="text">/<input type="text"></td>
-				<td><input type="text">/<input type="text"></td>
-			</tr>
-			<tr>
-				<td class="label_m"><label>6/3</label></td>
-				<td><input type="text"></td>
-				<td><input type="text">/<input type="text"></td>
-				<td><input type="text">/<input type="text"></td>
-				<td><input type="text">/<input type="text"></td>
-			</tr>
-			<tr>
-				<td class="label_m"><label>pv</label></td>
-				<td><input type="text"></td>
-				<td><input type="text"></td>
-				<td><input type="text"></td>
-				<td><input type="text"></td>
-			</tr>
-			<tr>
-				<td class="label_m"><label>yp</label></td>
-				<td><input type="text"></td>
-				<td><input type="text"></td>
-				<td><input type="text"></td>
-				<td><input type="text"></td>
-			</tr>
-			<tr>
-				<td class="label_m"><label>YS</label></td>
-				<td><input type="text"></td>
-				<td><input type="text"></td>
-				<td><input type="text"></td>
-				<td><input type="text"></td>
-			</tr>
-			<tr>
-				<td class="label_m"><label>GEL 10", 10', 30'</label></td>
-				<td><input type="text"></td>
-				<td><input type="text">/<input type="text">/<input type="text"></td>
-				<td><input type="text">/<input type="text">/<input type="text"></td>
-				<td><input type="text">/<input type="text">/<input type="text"></td>
-			</tr>	
-		</table>
-	</fieldset>
+	
 
-	<fieldset>
-		<legend>Químicas</legend>
-		<table>
-			<tr>
-				<td></td>
-				<td class="label_m"><label>Program</label></td>
-				<td class="label_m"><label>6:00</label></td>
-				<td class="label_m"><label>14:00</label></td>
-				<td class="label_m"><label>23:00</label></td>
-			</tr>
-			<tr>
-				<td>
-					<td class="label_m"><label>pH METER</label></td>	
-				</td>
-				<td><input type="text"></td>
-				<td><input type="text"></td>
-				<td><input type="text"></td>
-				<td><input type="text"></td>
-			</tr>
-			<tr>
-				<td>
-					<td class="label_m"><label>PM</label></td>	
-				</td>
-				<td><input type="text"></td>
-				<td><input type="text"></td>
-				<td><input type="text"></td>
-				<td><input type="text"></td>
-			</tr>
-			<tr>
-				<td>
-					<td class="label_m"><label>PF/MF</label></td>	
-				</td>
-				<td><input type="text"></td>
-				<td><input type="text">/<input type="text"></td>
-				<td><input type="text">/<input type="text"></td>
-				<td><input type="text">/<input type="text"></td>
-			</tr>
-			<tr>
-				<td>
-					<td class="label_m"><label>MBT</label></td>	
-				</td>
-				<td><input type="text"></td>
-				<td><input type="text"></td>
-				<td><input type="text"></td>
-				<td><input type="text"></td>
-			</tr>
-			<tr>
-				<td>
-					<td class="label_m"><label>CHLORIDES</label></td>	
-				</td>
-				<td><input type="text"></td>
-				<td><input type="text"></td>
-				<td><input type="text"></td>
-				<td><input type="text"></td>
-			</tr>
-			<tr>
-				<td>
-					<td class="label_m"><label>Ca++</label></td>	
-				</td>
-				<td><input type="text"></td>
-				<td><input type="text"></td>
-				<td><input type="text"></td>
-				<td><input type="text"></td>
-			</tr>
-			<tr>
-				<td>
-					<td class="label_m"><label>% water</label></td>	
-				</td>
-				<td><input type="text"></td>
-				<td><input type="text"></td>
-				<td><input type="text"></td>
-				<td><input type="text"></td>
-			</tr>
-			<tr>
-				<td>
-					<td class="label_m"><label>% oil</label></td>	
-				</td>
-				<td><input type="text"></td>
-				<td><input type="text"></td>
-				<td><input type="text"></td>
-				<td><input type="text"></td>
-			</tr>
-		</table>
-	</fieldset>
-	<fieldset>
-		<legend>Others</legend>
-		<table>
-			<tr>
-				<td class="label_m"><label>MUD PROPERTIES<label></td>
-				<td class="label_m"><label>PROGRAM</label></td>
-				<td class="label_m"><label>6:00</label></td>
-				<td class="label_m"><label>14:00</label></td>
-				<td class="label_m"><label>23:00</label></td>
-			</tr>
-			<tr>
-				<td><input type="text" /></td>
-				<td><input type="text" /></td>
-				<td><input type="text" /></td>
-				<td><input type="text" /></td>
-				<td><input type="text" /></td>
-			</tr>
-			<tr>
-				<td><input type="text" /></td>
-				<td><input type="text" /></td>
-				<td><input type="text" /></td>
-				<td><input type="text" /></td>
-				<td><input type="text" /></td>
-			</tr>
-			<tr>
-				<td><input type="text" /></td>
-				<td><input type="text" /></td>
-				<td><input type="text" /></td>
-				<td><input type="text" /></td>
-				<td><input type="text" /></td>
-			</tr>
-		</table>
-	</fieldset>
+	
+	
 </div>
