06107472d6dc2be9b2c00c31c76564c74a63f5d9
José Daniel Paternina <josedaniel.paterninasoto@gmail.com>
2012-07-04 07:39:53 -0500

    modificacion minima formatos de entrada

--- application/views/bomba.php
+++ application/views/bomba.php
@@ -1,57 +1,56 @@
 <div class="this_panel" id="bomba">
 	<h2>Datos de la Bomba</h2>
-	<table class="super_grid">
-		<tr>
-			<td style="width:470px">
-				<!-- MUD PUMP -->
-				<fieldset>
-					<legend>Mud pump</legend>
-					<table>
-						<tr>
-							<td></td>
-							<td class="label_m"><label>PUMP 1:</label></td>
-							<td class="label_m"><label>PUMP 2:</label></td>
-							<td class="label_m"><label>PUMP 3:</label></td>
-							
-						</tr>
-						<tr>
-							<td class="label_m"><label>LINER/STK:</label></td>
-							<td><input type="text" /></td>
-							<td><input type="text" /></td>
-							<td><input type="text" /></td>
-							
-						</tr>
-						<tr>
-							<td class="label_m"><label>GAL/STK:</label></td>
-							<td><input type="text" /></td>
-							<td><input type="text" /></td>
-							<td><input type="text" /></td>
-							
-						</tr>
-						<tr>
-							<td class="label_m"><label>SPM/STK:</label></td>
-							<td><input type="text" /></td>
-							<td><input type="text" /></td>
-							<td><input type="text" /></td>
-							
-						</tr>
-						<tr>
-							<td class="label_m"><label>GAL:</label></td>
-							<td><input type="text" /></td>
-							<td><input type="text" /></td>
-							<td><input type="text" /></td>
-							
-						</tr>
-						<tr>
-							<td></td>
-							<td></td>
-							<td class="label_m"><label>TOTAL GAL:</label></td>
-							<td><input type="text" /></td>
-						</tr>
-					</table>
-				</fieldset>
-			</td>
-			<td></td>
-		</tr>
-	</table>
+	<div class="simpleTabs">
+		<ul class="simpleTabsNavigation">
+	      <li><a href="#">Mud Pump</a></li>
+	    </ul>
+	    <div class="simpleTabsContent">
+	    	<!-- MUD PUMP -->
+			<fieldset>
+				<table>
+					<tr>
+						<td></td>
+						<td class="label_m"><label>PUMP 1:</label></td>
+						<td class="label_m"><label>PUMP 2:</label></td>
+						<td class="label_m"><label>PUMP 3:</label></td>
+						
+					</tr>
+					<tr>
+						<td class="label_m"><label>LINER/STK:</label></td>
+						<td><input type="text" /></td>
+						<td><input type="text" /></td>
+						<td><input type="text" /></td>
+						
+					</tr>
+					<tr>
+						<td class="label_m"><label>GAL/STK:</label></td>
+						<td><input type="text" /></td>
+						<td><input type="text" /></td>
+						<td><input type="text" /></td>
+						
+					</tr>
+					<tr>
+						<td class="label_m"><label>SPM/STK:</label></td>
+						<td><input type="text" /></td>
+						<td><input type="text" /></td>
+						<td><input type="text" /></td>
+						
+					</tr>
+					<tr>
+						<td class="label_m"><label>GAL:</label></td>
+						<td><input type="text" /></td>
+						<td><input type="text" /></td>
+						<td><input type="text" /></td>
+						
+					</tr>
+					<tr>
+						<td></td>
+						<td></td>
+						<td class="label_m"><label>TOTAL GAL:</label></td>
+						<td><input type="text" /></td>
+					</tr>
+				</table>
+			</fieldset>
+	    </div>
+	</div>
 </div>
--- application/views/broca.php
+++ application/views/broca.php
@@ -1,60 +1,58 @@
 <div class="this_panel" id="broca">
 	<h2>Datos de la Broca</h2>
-
-	<table class="super_grid">
-		<tr>
-			<td style="width:470px;">
-				<!-- BIT -->
-				<fieldset style="height:201px;">
-					<legend>Bit</legend>
-					<table>
+	<div class="simpleTabs">
+		<ul class="simpleTabsNavigation">
+	      <li><a href="#">Bit</a></li>
+	    </ul>
+	    <div class="simpleTabsContent">
+			<!-- BIT -->
+			<fieldset>
+				<table>
+					<tr>
+						<td class="label_m"><label>Bit #:</label></td>
+						<td><input type="text" class="medium"></td>
+						
+						<td class="label_m"><label>Bit Diameter:</label></td>
+						<td><input type="text" class="medium"></td>
+					</tr>
+					<tr>
+						<td class="label_m"><label>Type:</label></td>
+						<td><input type="text" class="medium"></td>
+						
+						<td class="label_m"><label>Jets Number:</label></td>
+						<td><input type="text" class="medium"></td>
+					</tr>
+				</table>
+				<fieldset>
+					<legend>Jets</legend>
+					<table style="float:left;">
 						<tr>
-							<td class="label_m"><label>Bit #:</label></td>
-							<td><input type="text" class="medium"></td>
-							
-							<td class="label_m"><label>Bit Diameter:</label></td>
-							<td><input type="text" class="medium"></td>
+							<td><input type="text" placeholder="1"></td>
+							<td><input type="text" placeholder="2"></td>
+							<td><input type="text" placeholder="3"></td>
+							<td><input type="text" placeholder="4"></td>
 						</tr>
 						<tr>
-							<td class="label_m"><label>Type:</label></td>
-							<td><input type="text" class="medium"></td>
-							
-							<td class="label_m"><label>Jets Number:</label></td>
-							<td><input type="text" class="medium"></td>
+							<td><input type="text" placeholder="5"></td>
+							<td><input type="text" placeholder="6"></td>
+							<td><input type="text" placeholder="7"></td>
+							<td><input type="text" placeholder="8"></td>
+						</tr>
+						<tr>
+							<td><input type="text" placeholder="9"></td>
+							<td><input type="text" placeholder="10"></td>
+							<td><input type="text" placeholder="11"></td>
+							<td><input type="text" placeholder="12"></td>
 						</tr>
 					</table>
-					<fieldset>
-						<legend>Jets</legend>
-						<table style="float:left;">
-							<tr>
-								<td><input type="text" placeholder="1"></td>
-								<td><input type="text" placeholder="2"></td>
-								<td><input type="text" placeholder="3"></td>
-								<td><input type="text" placeholder="4"></td>
-							</tr>
-							<tr>
-								<td><input type="text" placeholder="5"></td>
-								<td><input type="text" placeholder="6"></td>
-								<td><input type="text" placeholder="7"></td>
-								<td><input type="text" placeholder="8"></td>
-							</tr>
-							<tr>
-								<td><input type="text" placeholder="9"></td>
-								<td><input type="text" placeholder="10"></td>
-								<td><input type="text" placeholder="11"></td>
-								<td><input type="text" placeholder="12"></td>
-							</tr>
-						</table>
-						<table style="float:right;margin-right:10px;">
-							<tr>
-								<td class="label_m"><label class="emphasis">TFA:</label></td>
-								<td class="label_m"><input type="text" disabled="disabled" style="margin-right:5px;width:55px;" /> in<sup>2</sup></td>
-							</tr>
-						</table>
-					</fieldset>
+					<table style="float:right;margin-right:10px;">
+						<tr>
+							<td class="label_m"><label class="emphasis">TFA:</label></td>
+							<td class="label_m"><input type="text" disabled="disabled" style="margin-right:5px;width:55px;" /> in<sup>2</sup></td>
+						</tr>
+					</table>
 				</fieldset>
-			</td>
-			<td></td>
-		</tr>
-	</table>
+			</fieldset>	    
+	    </div>
+	</div>
 </div>
