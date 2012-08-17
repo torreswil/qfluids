18910423b9eafc2b8bece6755283b3fedc7df93a
José Daniel Paternina <josedaniel.paterninasoto@gmail.com>
2012-08-17 16:18:19 -0500

    cambios mas recientes

--- application/views/project_settings.php
+++ application/views/project_settings.php
@@ -20,19 +20,19 @@
 	        	<fieldset>
 		        	<table>
 						<tr>
-							<td class="label_m" style="width:175px;"><label>Well Name:</label></td>
+							<td class="label_m" style="width:175px;"><label>Well Name*:</label></td>
 							<td><input type="text" style="width:150px;" value="<?= $project['well_name'] ?>" /></td>
 						</tr>
 						<tr>
-							<td class="label_m"><label>Operator:</label></td>
+							<td class="label_m"><label>Operator*:</label></td>
 							<td><input type="text" style="width:150px;" value="<?= $project['operator'] ?>" /></td>
 						</tr>
 						<tr>
-							<td class="label_m"><label>RIG:</label></td>
+							<td class="label_m"><label>RIG*:</label></td>
 							<td><input type="text" style="width:150px;" value="<?= $project['rig'] ?>" class="rigname_source" /></td>
 						</tr>
 						<tr>
-							<td class="label_m"><label>Location:</label></td>
+							<td class="label_m"><label>Location*:</label></td>
 							<td><input type="text" style="width:150px;" value="<?= $project['geozone'] ?>" /></td>
 						</tr>
 						<tr>
@@ -44,7 +44,7 @@
 							<td><input type="text" style="width:150px;" value="<?= $project['field'] ?>" /></td>
 						</tr>
 						<tr>
-							<td class="label_m"><label>Operator Logo:</label></td>
+							<td class="label_m"><label>Operator Logo*:</label></td>
 							<td><input type="text" style="width:150px;" placeholder="Click to select..." /></td>
 						</tr>	
 					</table>
