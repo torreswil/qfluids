929768d54443c326196821a7c5e1e00e5890204f
José Daniel Paternina <josedaniel.paterninasoto@gmail.com>
2012-09-11 12:09:40 -0500

    correcion cse

--- application/views/equipos_solidos.php
+++ application/views/equipos_solidos.php
@@ -212,7 +212,7 @@
             				<td class="label_m"><label>Bowl Diam.:</label></td>
             				<td class="label_m"><label>Bowl Pulley.<br />Diam.:</label></td>
             				<td class="label_m"><label>Motor Pulley.<br />Diam.:</label></td>
-            				<?php if($centrifuge['variator'] !== 1){?><td class="label_m"><label>Motor RPM:</label></td><?php } ?>
+            				<?php if($centrifugue['variator'] !== 1){?><td class="label_m"><label>Motor RPM:</label></td><?php } ?>
             				<td class="label_m"><label>Speed RPM:</label></td>
             				<td class="label_m"><label>G force:</label></td>
             				<td class="label_m"><label>Type:</label></td>
@@ -221,7 +221,7 @@
             				<td class="label_m">in.</td>
             				<td class="label_m">in.</td>
             				<td class="label_m">in.</td>
-            				<?php if($centrifuge['variator'] !== 1){?><td class="label_m"></td><?php } ?>
+            				<?php if($centrifugue['variator'] !== 1){?><td class="label_m"></td><?php } ?>
             				<td class="label_m"></td>
             				<td class="label_m"></td>
             				<td class="label_m"></td>
@@ -230,8 +230,8 @@
             				<td class="label_m"><input type="text" style="width:85px;" /></td>
             				<td class="label_m"><input type="text" style="width:85px;" /></td>
             				<td class="label_m"><input type="text" style="width:85px;" /></td>
-            				<?php if($centrifuge['variator'] !== 1){?><td class="label_m"><input type="text" style="width:85px;" /></td></td><?php } ?>
-                            <?php if($centrifuge['variator'] !== 0){ $disabled = 'disabled="disabled"'; }else{ $disabled = ''; } ?>
+            				<?php if($centrifugue['variator'] !== 1){?><td class="label_m"><input type="text" style="width:85px;" /></td></td><?php } ?>
+                            <?php if($centrifugue['variator'] !== 0){ $disabled = 'disabled="disabled"'; }else{ $disabled = ''; } ?>
                             <td class="label_m"><input type="text" style="width:85px;" <?= $disabled ?> /></td>
             				<td class="label_m"><input type="text" style="width:85px;" disabled="disabled" /></td> 
             				<td class="label_m"><input type="text" style="width:85px;" disabled="disabled" /></td>
