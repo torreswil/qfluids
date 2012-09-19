ae2f56734ec366a749f6a6357f9c05f7e7f8a3e6
Iván D. Meléndez <argordmel@yahoo.es>
2012-09-19 16:05:59 -0500

    Se separan algunas configuraciones que se juntaron por error

---
+++ application/views/settings/tools_and_mud.php
@@ -0,0 +1,21 @@
+<div class="config_panel" id="tools_and_mud">
+	<h2>Tools and mud librarys</h2>
+	
+	<div class="simpleTabs">
+            <ul class="simpleTabsNavigation">
+                <li><a href="#">Bit</a></li>
+                <li><a href="#">Pump</a></li>
+                <li><a href="#">Casing</a></li>
+                <li><a href="#">Mud</a></li>
+	    </ul>
+            
+            <div class="simpleTabsContent" style="top:65px;border-bottom:1px solid #E0E0E0;">
+        	<fieldset>
+                    <legend>Bits list</legend>                    
+                    <table id="current_bits_list"></table>
+        	</fieldset>
+	    </div>
+            
+	</div>
+
+</div>
--- js/config_panel.js
+++ js/config_panel.js
@@ -566,23 +566,7 @@ $(function(){
 		}else{
 			alert('Some fields are wrong. Please verify and try again');
 		}
-	});
-        
-        //remove an bit - IvanMel
-	$('.remove_bit_link').live('click',function(e){
-		e.preventDefault();
-		var id = $(this).attr('id');
-		id = id.split('rm_bit_')[1];
-		
-                var data = {'id':id};
-
-		$.post('/rest/remove_bit',data,function(r){
-			if(r.message == 'deactivated'){
-                            load_personal();
-                            $('#close_settings_btn').val('Close & Reload').removeClass('just_close');
-			}
-		},'json');
-	});
+	});                
 
 	function load_personal(){
 		//load enginers
@@ -618,10 +602,36 @@ $(function(){
 			$('#current_yardworkers_list').html(r);
 		});	
                 
-                //Load bits - IvanMel
+	}
+        
+        /*==========================================================================================================*/
+	// 7. TOOLS AND MUD LIBRARY - IvanMel
+	/*==========================================================================================================*/
+	
+	//when opening the tools and mud library panel...
+	$('#tools_settings_link').click(function(e){
+		e.preventDefault();                
+		load_tools_and_mud();
+	})
+	  
+	$('.remove_bit_link').live('click',function(e){
+		e.preventDefault();
+		var id = $(this).attr('id');
+		id = id.split('rm_bit_')[1];
+		
+                var data = {'id':id};
+
+		$.post('/rest/remove_bit',data,function(r){
+			if(r.message == 'deactivated'){
+                            load_tools_and_mud();
+                            $('#close_settings_btn').val('Close & Reload').removeClass('just_close');
+			}
+		},'json');
+	});
+
+	function load_tools_and_mud() {                
                 var bit_data = {
-                    project 	:$('#project_id').val(),
-                    type 		:'bit', 
+                    project             :$('#project_id').val(),                    
                     active		: 1
                 };
                 
@@ -629,6 +639,5 @@ $(function(){
 			$('#current_bits_list').html(r);
 		});
 	}
-		
 });
 /****** THE END ******/
