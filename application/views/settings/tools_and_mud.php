d47dcd3a50505e556d4c3f93b9787f61cc1ce8af
Iván D. Meléndez <argordmel@yahoo.es>
2012-09-19 18:32:54 -0500

    Se carga la parte de tools and mud library

--- application/views/settings/tools_and_mud.php
+++ application/views/settings/tools_and_mud.php
@@ -12,14 +12,90 @@
             <div class="simpleTabsContent" style="top:65px;border-bottom:1px solid #E0E0E0;">
         	<fieldset>
                     <legend>Bits list</legend>                    
-                    <table id="current_bits_list"></table>
+                    <table>
+                        <thead style="display: none;">
+                            <tr>                                
+                                <th style="text-align: left">Name</th>
+                                <th style="text-align: left">Model</th>
+                                <th style="text-align: left">Od/Frac</th>
+                                <th style="text-align: left">Unit</th>
+                                <th style="text-align: left">Od/Deci</th>
+                                <th style="text-align: left">Unit</th>                               
+                                <th style="text-align: left">Length</th>
+                                <th style="text-align: left">Unit</th>
+                                
+                                <th></th>
+                            </tr>                            
+                        </thead>                            
+                        <tbody  id="current_bits_list"></tbody>
+                    </table>
         	</fieldset>
 	    </div>
             
             <div class="simpleTabsContent" style="top:65px;border-bottom:1px solid #E0E0E0;">
         	<fieldset>
-                    <legend>Muds list</legend>                    
-                    <table id="current_muds_list"></table>
+                    <legend>Pumps list</legend> 
+                    <table>
+                        <thead style="display: none;">
+                            <tr>                                
+                                <th style="text-align: left">Maker</th>
+                                <th style="text-align: left">Type</th>
+                                <th style="text-align: left">Model</th>
+                                <th style="text-align: left">Stroke</th>
+                                <th style="text-align: left">Unit</th>
+                                <th style="text-align: left">Diameter</th>
+                                <th style="text-align: left">Unit</th>
+                                <th style="text-align: left">Rod</th>
+                                <th style="text-align: left">Unit</th>
+                                <th style="text-align: left">Presion</th>
+                                <th style="text-align: left">Unit</th>                            
+                                <th style="text-align: left">Max spm</th> 
+                                <th style="text-align: left">S. Frac</th> 
+                                <th style="text-align: left">Unit</th> 
+                                <th style="text-align: left">D. Frac</th> 
+                                <th style="text-align: left">Unit</th> 
+                                <th style="text-align: left">R. Frac</th>
+                                <th style="text-align: left">Unit</th>
+                                <th></th>
+                            </tr>                            
+                        </thead>                        
+                        <tbody id="current_pumps_list"></tbody>
+                    </table>                      
+        	</fieldset>
+	    </div>
+            
+            <div class="simpleTabsContent" style="top:65px;border-bottom:1px solid #E0E0E0;">
+        	<fieldset>
+                    <legend>Casings list</legend>    
+                    <table>
+                        <thead  style="display: none;">
+                            <tr>
+                                <th style="text-align: left">Od/Frac</th>
+                                <th style="text-align: left">Od/Deci</th>
+                                <th style="text-align: left">Unit</th>
+                                <th style="text-align: left">Id/Frac</th>
+                                <th style="text-align: left">Id/Deci</th>
+                                <th style="text-align: left">Unit</th>
+                                <th style="text-align: left">Weight</th>
+                                <th colspan="2" style="text-align: left">Unit</th>
+                            </tr>                            
+                        </thead>                        
+                        <tbody id="current_casings_list"></tbody>
+                    </table>                    
+        	</fieldset>
+	    </div>
+            
+            <div class="simpleTabsContent" style="top:65px;border-bottom:1px solid #E0E0E0;">
+        	<fieldset>
+                    <legend>Muds list</legend>  
+                    <table>
+                        <thead style="display: none;">
+                            <tr>
+                                <th colspan="2" style="text-align: left">Name</th>                                                       
+                            </tr>                            
+                        </thead>                        
+                        <tbody id="current_muds_list"></tbody>
+                    </table>                    
         	</fieldset>
 	    </div>
             
