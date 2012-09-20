4a69f4a04d6a6bb7e48842208266bef3dad7f327
Iván D. Meléndez <argordmel@yahoo.es>
2012-09-20 17:09:56 -0500

    Se visualiza el reporte de los ingenieros

--- application/views/enginer_report.php
+++ application/views/enginer_report.php
@@ -1,38 +1,31 @@
 <div style="margin:0 auto; width:800px;">
-	<h1 style="color:#259271;text-transform:uppercase;margin-top:50px;"><?= $enginer['name']?> <?= $enginer['lastname']	?> - <?= $enginer['identification']	?></h1>
-	<h2 style="color:#666;text-transform:uppercase;margin-bottom:50px;">WORK REPORT FOR <?= $this->session->userdata['project']['well_name'] ?> (<?= $this->session->userdata['project']['operator'] ?>)</h2>
+	<h2 style="color:#259271;text-transform:uppercase;margin-top:50px;"><?= $enginer['name']?> <?= $enginer['lastname']	?> - <?= $enginer['identification']	?></h2>
+	<h3 style="color:#666;text-transform:uppercase;margin-bottom:50px;">WORK REPORT FOR <?= $this->session->userdata['project']['well_name'] ?> (<?= $this->session->userdata['project']['operator'] ?>)</h3>
 
 	<?php if(count($periods) > 0){ ?>
-
-	<table class="datatable" style="width:800px">
-		<thead>
-			<tr>
-				<td>Well</td><td>Date</td><td>Period</td><td>Registered at</td>
-			</tr>
-		</thead>
-		<tbody>
-			<?php foreach($periods as $period){ ?>
-			<tr style="cursor:default;">
-				<td><?= $this->session->userdata['project']['well_name'] ?></td>
-				<td>
-					<?php 
-						$dia = date_create($period['timestamp']);
-						echo date_format($dia, 'Y-m-d');
-					?>
-				</td>
-				<td>
-					<?php $period['period'] == '1' ? $period_st = '06:00 - 17:59' : $period_st = '18:00 - 5:59'; ?>
-					<?= $period_st; ?>
-				</td>
-				<td>
-					<?php 
-						$hora = date_create($period['timestamp']);
-						echo date_format($hora, 'H:i:s');
-					?>
-				</td>
-			</tr>
-			<?php } ?>
-		</tbody>
-	</table>
+        <fieldset>                
+                <table class="datatable" style="width:800px">
+                        <thead>
+                                <tr>
+                                        <td>Well</td>
+                                        <td>Operator</td>
+                                        <td>Date</td>
+                                        <td>Rate</td>
+                                        <td>Cover</td>
+                                </tr>
+                        </thead>
+                        <tbody>
+                                <?php foreach($periods as $period){ ?>
+                                <tr style="cursor:default;">
+                                        <td><?php echo $period['well_name']; ?></td>
+                                        <td><?php echo $period['operator']; ?></td>
+                                        <td><?php echo date("Y-m-d", strtotime($period['date']));?></td>
+                                        <td><?php echo $period['rate']; ?></td>
+                                        <td><?php echo ($period['cover'] == '1') ? 'YES' : 'NO';?></td>
+                                </tr>
+                                <?php } ?>
+                        </tbody>
+                </table>
+        </fieldset>
 	<?php } ?>
 </div>
