4109b5faa20676f3c09f64fbda9ef60b4f92d781
José Daniel Paternina <josedaniel.paterninasoto@gmail.com>
2012-09-24 20:44:57 -0500

    cambiando el orden de los formularios de tanques

--- application/views/settings/tanks.php
+++ application/views/settings/tanks.php
@@ -3,6 +3,28 @@
 		<h2 style="float:left;width:70px;">Tanks</h2>
 		<a href="#create_tank" id="link_create_tank" style="float:left;width:100px;text-decoration:underline;margin-top:3px;">[+ Add Tank]</a>
 	</div>
+
+	<fieldset style="float:left;width:95%;">
+		<legend>Current trip tanks</legend>
+		<table>
+			<thead>
+    			<tr>
+    				<td></td>
+    				<td class="label_m"><label>Order</label></td>
+    				<td class="label_m"><label>TANK NAME:</label></td>
+    				<td class="label_m"><label>AGITATORS #:</label></td>
+    				<td class="label_m"><label>JETS:</label></td>
+    				<td class="label_m"><label>TANK TYPE:</label></td>
+    				<td class="label_m"><label>VOL. CAP. (BBL):</label></td>
+    				<td class="label_m"><label>MAX HEADROOM:</label></td>
+    			</tr>
+			</thead>
+			<tbody id="current_trip_tanks">
+				
+			</tbody>
+		</table>
+		<input type="button" value="Update order" class="update_tank_order" id="trip_order" />
+	</fieldset>
 	
 	<fieldset style="float:left;width:95%;">
 		<legend>Current active tanks:</legend>
@@ -27,50 +49,6 @@
 	</fieldset>
 
 	<fieldset style="float:left;width:95%;">
-		<legend>Current reserve tanks</legend>
-		<table>
-			<thead>
-    			<tr>
-    				<td></td>
-    				<td class="label_m"><label>Order</label></td>
-    				<td class="label_m"><label>TANK NAME:</label></td>
-    				<td class="label_m"><label>AGITATORS #:</label></td>
-    				<td class="label_m"><label>JETS:</label></td>
-    				<td class="label_m"><label>TANK TYPE:</label></td>
-    				<td class="label_m"><label>VOL. CAP. (BBL):</label></td>
-    				<td class="label_m"><label>MAX HEADROOM:</label></td>
-    			</tr>
-			</thead>
-			<tbody id="current_reserve_tanks">
-				
-			</tbody>
-		</table>
-		<input type="button" value="Update order" class="update_tank_order" id="reserve_order" />
-	</fieldset>
-
-	<fieldset style="float:left;width:95%;">
-		<legend>Current trip tanks</legend>
-		<table>
-			<thead>
-    			<tr>
-    				<td></td>
-    				<td class="label_m"><label>Order</label></td>
-    				<td class="label_m"><label>TANK NAME:</label></td>
-    				<td class="label_m"><label>AGITATORS #:</label></td>
-    				<td class="label_m"><label>JETS:</label></td>
-    				<td class="label_m"><label>TANK TYPE:</label></td>
-    				<td class="label_m"><label>VOL. CAP. (BBL):</label></td>
-    				<td class="label_m"><label>MAX HEADROOM:</label></td>
-    			</tr>
-			</thead>
-			<tbody id="current_trip_tanks">
-				
-			</tbody>
-		</table>
-		<input type="button" value="Update order" class="update_tank_order" id="trip_order" />
-	</fieldset>
-
-	<fieldset style="float:left;width:95%;">
 		<legend>Current pill tanks</legend>
 		<table>
 			<thead>
@@ -90,6 +68,28 @@
 			</tbody>
 		</table>
 		<input type="button" value="Update order" class="update_tank_order" id="pill_order" />
+	</fieldset>
+
+	<fieldset style="float:left;width:95%;">
+		<legend>Current reserve tanks</legend>
+		<table>
+			<thead>
+    			<tr>
+    				<td></td>
+    				<td class="label_m"><label>Order</label></td>
+    				<td class="label_m"><label>TANK NAME:</label></td>
+    				<td class="label_m"><label>AGITATORS #:</label></td>
+    				<td class="label_m"><label>JETS:</label></td>
+    				<td class="label_m"><label>TANK TYPE:</label></td>
+    				<td class="label_m"><label>VOL. CAP. (BBL):</label></td>
+    				<td class="label_m"><label>MAX HEADROOM:</label></td>
+    			</tr>
+			</thead>
+			<tbody id="current_reserve_tanks">
+				
+			</tbody>
+		</table>
+		<input type="button" value="Update order" class="update_tank_order" id="reserve_order" />
 	</fieldset>
 	
 </div>
