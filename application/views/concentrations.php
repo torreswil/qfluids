36788a8c92f9d6e890392efe5d034c11fefcfe36
José Daniel Paternina <josedaniel.paterninasoto@gmail.com>
2012-10-10 09:13:54 -0500

    tabla de concentraciones

--- application/views/concentrations.php
+++ application/views/concentrations.php
@@ -1,3 +1,54 @@
 <div class="this_panel" id="concentrations">
+	<h2>Concentrations</h2>
 
+     <div class="simpleTabs">
+          <ul class="simpleTabsNavigation">
+               <li><a href="#">Status</a></li>            
+          </ul>
+          <div class="simpleTabsContent" style="border-bottom:1px solid #E0E0E0;">
+			<fieldset>
+				<legend>Material Concentrations status per Tank</legend>
+				<table>
+				     <thead>
+				          <tr>
+				            <td class="label_m"><label>Material</label></td>
+				            <td class="label_m"><label>Unit</label></td>
+				            <td class="label_m"><label>U. Cost</label></td>
+				            <td class="label_m"><label>Active</label></td>
+				            <?php foreach($pill_tanks as $tank){ ?>
+				              <td class="label_m"><label><?= $tank['tank_name'] ?></label></td>
+				            <?php }?>
+				            <?php foreach($reserve_tanks as $tank){ ?>
+				              <?php if($tank['name'] < 32){ ?>
+				                <td class="label_m"><label><?= $tank['tank_name'] ?></label></td>
+				              <?php } ?>
+				            <?php }?>
+				            <td class="label_m"><label>T. Consu.</label></td>
+				            <td class="label_m"><label>T. Cost</label></td>
+				          </tr>
+				     </thead>
+				     <tbody class="materials_table">
+				        <?php foreach ($materials as $material) { ?>
+				            <tr class="this_material_<?= $material['product_id']?> ">
+				                <td><input style="width:200px;max-width:357px;margin-right:0;" type="text" disabled="disabled" value="<?= $material['commercial_name'] ?>" /></td>
+				                <td><input style="width:55px;margin-right:0;" type="text" disabled="disabled" value="<?= $material['equivalencia'] ?> <?= $material['unidad_destino'] ?>" /></td>
+				                <td><input type="text" style="width:55px;margin-right:0;" value="<?= $material['price'] ?>" disabled /></td>
+				                <td><input type="text" style="width:55px;margin-right:0;" disabled /></td>
+				                <?php foreach($pill_tanks as $tank){ ?>
+				                  <td><input type="text" style="width:55px;margin-right:0;" disabled /></td>
+				                <?php }?>
+				                <?php foreach($reserve_tanks as $tank){ ?>
+				                  <?php if($tank['name'] < 32){ ?>
+				                    <td><input type="text" style="width:55px;margin-right:0;" disabled /></td>
+				                  <?php } ?>
+				                <?php }?>
+				                <td><input type="text" style="width:55px;margin-right:0;" disabled /></td>
+				                <td><input type="text" style="width:55px;margin-right:0;" disabled /></td>
+				            </tr>
+				        <?php } ?>
+				     </tbody>
+				</table>
+			</fieldset> 
+		</div>
+	</div>
 </div>
--- application/views/inventario.php
+++ application/views/inventario.php
@@ -89,11 +89,13 @@
             </div>
             <div class="simpleTabsContent" style="border-bottom:1px solid #E0E0E0;">
               <fieldset>
+                <legend>Material Consumptions for <?php if($project['spud_date'] != ''){ echo $project['last_report_meta']['date'];} ?> (Report <?= str_pad($project['last_report'], 4, "0", STR_PAD_LEFT); ?>)</legend>
                 <table>
                      <thead>
                           <tr>
                             <td class="label_m"><label>Material</label></td>
                             <td class="label_m"><label>Unit</label></td>
+                            <td class="label_m"><label>U. Cost</label></td>
                             <td class="label_m"><label>Active</label></td>
                             <?php foreach($pill_tanks as $tank){ ?>
                               <td class="label_m"><label><?= $tank['tank_name'] ?></label></td>
@@ -103,14 +105,17 @@
                                 <td class="label_m"><label><?= $tank['tank_name'] ?></label></td>
                               <?php } ?>
                             <?php }?>
+                            <td class="label_m"><label>T. Consu.</label></td>
+                            <td class="label_m"><label>T. Cost</label></td>
                           </tr>
                      </thead>
                      <tbody class="materials_table">
                         <?php foreach ($materials as $material) { ?>
                             <tr class="this_material_<?= $material['product_id']?> ">
                                 <td><input style="width:200px;max-width:357px;margin-right:0;" type="text" disabled="disabled" value="<?= $material['commercial_name'] ?>" /></td>
-                                <td><input style="width:100px;margin-right:0;" type="text" disabled="disabled" value="<?= $material['equivalencia'] ?> <?= $material['unidad_destino'] ?>" /></td>
-                                <td class="label_m"><input type="text" style="width:55px;margin-right:0;" disabled /></td>
+                                <td><input style="width:55px;margin-right:0;" type="text" disabled="disabled" value="<?= $material['equivalencia'] ?> <?= $material['unidad_destino'] ?>" /></td>
+                                <td><input type="text" style="width:55px;margin-right:0;" value="<?= $material['price'] ?>" disabled /></td>
+                                <td><input type="text" style="width:55px;margin-right:0;" disabled /></td>
                                 <?php foreach($pill_tanks as $tank){ ?>
                                   <td><input type="text" style="width:55px;margin-right:0;" disabled /></td>
                                 <?php }?>
@@ -119,6 +124,8 @@
                                     <td><input type="text" style="width:55px;margin-right:0;" disabled /></td>
                                   <?php } ?>
                                 <?php }?>
+                                <td><input type="text" style="width:55px;margin-right:0;" disabled /></td>
+                                <td><input type="text" style="width:55px;margin-right:0;" disabled /></td>
                             </tr>
                         <?php } ?>
                      </tbody>
